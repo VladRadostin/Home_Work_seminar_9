580b22665c7931d7fc4d481a106fcda9b4ea5c91
vlad <vladradostinn@yandex.ru>
2023-07-09 03:33:17 +0300

    Home_Work_seminar_9

---
+++ Home_Work_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void SumaNaturalNumbers(int count,int count2, int sum)
+{
+    Console.Write($"{count} ");
+    if(count != count2)
+    {
+        SumaNaturalNumbers(count = count - 1, count2, sum = sum + count);
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Сумма элементов {sum} ");
+    }
+}
+
+int m = 1;
+int n = 15;
+SumaNaturalNumbers(n , m, n);
+
+
+Console.WriteLine();
---
+++ Home_Work_3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int A(int m1, int n1)
+{
+    int result = 0;
+
+    if(m1 == 0)
+    {
+        result = n1 + 1;
+    }
+    else if(m1 > 0 & n1 == 0)
+    {
+        result = A(m1 - 1, 1);
+    }
+    else if(m1 > 0 & n1 > 0)
+    {
+        result = A(m1 - 1, A(m1, n1-1));
+    }
+
+    return result;
+
+}
+
+int m = 3;
+int n = 2;
+
+Console.WriteLine(A(m, n));
